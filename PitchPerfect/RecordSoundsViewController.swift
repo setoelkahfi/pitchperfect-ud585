c3b4d9c3f2c4b7cad211d4a639a352716fc3a678
Seto Elkahfi <setoelkahfi@gmail.com>
2016-12-20 12:38:38 +0700

    Add PlaySoundsViewController to playback the recorded audio

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -70,5 +70,12 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
 }
 
